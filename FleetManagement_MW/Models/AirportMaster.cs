367649bf2c787b04f01d5baf4af4c78bfd1152b1
amitcdacvita <133513750+amitcdacvita@users.noreply.github.com>
2023-08-28 18:39:08 +0530

    done by ramizee airportmaster, hubmaster
    airportmaster and hubmaster

---
+++ FleetManagement_MW/Models/AirportMaster.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FleetManagement_MW.Models
+{
+    public class AirportMaster
+    {
+        [Key] 
+        public long airportId { get; set; }
+        public string airportName { get; set;}
+        public long cityId { get; set; }
+        public long stateId { get; set; }
+        public long hubId { get; set; } 
+        public long airportCode { get; set; }
+        public string address { get; set; }
+        public string openingTime { get; set; }
+        public string closingTime { get; set; }
+        public CityMaster city { get; set; }
+        public StateMaster state { get; set; }
+        public HubMaster hub { get; set; }
+    }
+}
---
+++ FleetManagement_MW/Models/HubMaster.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FleetManagement_MW.Models
+{
+    public class HubMaster
+    {
+        [Key]
+        public long hubId { get; set; }
+        public string hubName { get; set; }
+public string hubAddress { get; set; }
+        public long cityId { get; set; }
+        public long stateId { get; set; }
+        public string hubPhoneNo { get; set; }
+        public string openingHours { get; set; }
+        public StateMaster state { get; set; }
+        public CityMaster city { get; set; }    
+    }
+}
